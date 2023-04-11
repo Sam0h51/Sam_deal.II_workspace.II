9ebe87ecdd1d4bcc9726c592de281eaa818a996e
Ubuntu <uneake0h51@gmail.com>
2023-04-11 23:56:11 +0000

    Changed a lot. Started working on full SH problem. Had lots of bugs, but now I think the code is roughly correct. Did some convergence testing, seems like the timestep may be important, some pngs included. Hopefully this isn't too big to push

---
+++ examples/20-[00]-first-pass/step-20/CMakeFiles/step-20.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/step-20.dir/step-20.cc.o"
+  "step-20"
+  "step-20.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/step-20.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ examples/[00]-starting-SH-code/step-26/CMakeFiles/00-SH-full-equation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "00-SH-full-equation"
+  "00-SH-full-equation.pdb"
+  "CMakeFiles/00-SH-full-equation.dir/00-SH-full-equation.cc.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/00-SH-full-equation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
